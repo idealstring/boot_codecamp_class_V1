a721841def31f05ab43e46167c055d3bcbd06783
idealstring <idealstring@gmail.com>
2022-08-30 09:05:44 +0900

    프리보드 등록하기 페이지

---
+++ freeboard_frontend/styles/write.js
@@ -0,0 +1,181 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  box-sizing: border-box;
+  margin: 0 auto;
+  padding: 60px 100px 100px 100px;
+  width: 1200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: space-between;
+  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.2);
+`;
+
+export const Title = styled.div`
+  margin-bottom: 40px;
+  text-align: center;
+  font-size: 36px;
+  font-weight: 700px;
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  algin-items: center;
+`;
+
+export const WriterPwdWrapper = styled.div`
+  margin-bottom: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  algin-items: center;
+`;
+
+export const WriterPwd = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  algin-items: flex-start;
+`;
+
+export const Subtitle = styled.div`
+  font-weight: 500;
+  margin-bottom: 16px;
+`;
+
+export const InputW486px = styled.input`
+  padding: 16px;
+  width: 486px;
+  height: 52px;
+  border: 1px solid #bdbdbd;
+  color: #c4c4c4;
+`;
+
+export const SubtitleWrapper = styled.div`
+  margin-bottom: 40px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const InputW100per = styled.input`
+  //   margin-bottom: 16px;
+  padding: 16px;
+  width: 100%;
+  height: 52px;
+  border: 1px solid #bdbdbd;
+  color: #c4c4c4;
+`;
+
+export const InputW100perM16px = styled.input`
+  margin: 16px 0 16px 0;
+  padding: 16px;
+  width: 100%;
+  height: 52px;
+  border: 1px solid #bdbdbd;
+  color: #c4c4c4;
+`;
+
+export const TextareaW100perH480px = styled.textarea`
+  padding: 16px;
+  width: 100%;
+  height: 480px;
+  border: 1px solid #bdbdbd;
+  color: #c4c4c4;
+  text-top: top;
+`;
+export const InputW77pxH52px = styled.input`
+  padding: 16px;
+  width: 77px;
+  height: 52px;
+  border: 1px solid #bdbdbd;
+  color: #c4c4c4;
+`;
+
+export const PostButton = styled.button`
+  margin-left: 16px;
+  padding: 0 16px;
+  width: 124px;
+  height: 52px;
+  border: none;
+  font-weight: 500;
+  color: white;
+  background: black;
+  cursor: pointer;
+`;
+
+export const InputRadio = styled.input`
+  appearance: none;
+  margin: 0 5px;
+  &:after {
+    width: 14px;
+    height: 14px;
+    border-radius: 15px;
+    top: 1px;
+    left: -5px;
+    position: relative;
+    background-color: #ffffff;
+    content: "";
+    display: inline-block;
+    visibility: visible;
+    border: 1px solid #000000;
+  }
+  &:checked:after {
+    width: 14px;
+    height: 14px;
+    border-radius: 15px;
+    top: 1px;
+    left: -5px;
+    position: relative;
+    background-color: ##ffd600;
+    content: "";
+    display: inline-block;
+    visibility: visible;
+    border: 1px solid #ffd600;
+  }
+`;
+
+export const RadioLabel = styled.label`
+  margin-right: 20px;
+`;
+
+export const SubmitButtonWrapper = styled.div`
+  background-color: none;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  button {
+    padding: 0 16px;
+    width: 179px;
+    height: 52px;
+    border: none;
+    font-weight: 500;
+    color: #000000;
+    background: #ffd600;
+    cursor: pointer;
+  }
+`;
+
+export const ImgUploadWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+export const ImgUpload = styled.div`
+  margin-right: 24px;
+  width: 78px;
+  height: 78px;
+  background-color: #bdbdbd;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #4f4f4f;
+  font-size: 24px;
+  cursor: pointer;
+  span {
+    font-size: 12px;
+    font-weight: 500;
+  }
+`;
