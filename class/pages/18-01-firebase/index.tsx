51fd467878451d0677e36b0737c96bbcf050beb9
idealstring <idealstring@gmail.com>
2022-09-25 13:08:09 +0900

    18일차 class firebase

---
+++ class/pages/18-01-firebase/index.tsx
@@ -0,0 +1,33 @@
+import {
+  collection,
+  addDoc,
+  getDocs,
+  getFirestore,
+  getDoc,
+} from "firebase/firestore/lite";
+import { firebaseApp } from "../_app";
+
+export default function FirebasePage() {
+  const onClickSubmit = () => {
+    const board = collection(getFirestore(firebaseApp), "board");
+    void addDoc(board, {
+      writer: "코난",
+      title: "아침에 눈을 뜨면",
+      contents: "지난 밤이 궁금해",
+    });
+  };
+  const onClickFetch = async () => {
+    const board = collection(getFirestore(firebaseApp), "board");
+    const result = await getDocs(board);
+    const datas = result.docs.map((el) => el.data()); // 데이터가 배열로 묶여있어 map으로 뽑아줘야함.
+
+    console.log(datas);
+  };
+
+  return (
+    <>
+      <button onClick={onClickSubmit}>등록하기</button>
+      <button onClick={onClickFetch}>조회하기</button>
+    </>
+  );
+}
--- class/pages/_app.tsx
+++ class/pages/_app.tsx
@@ -6,6 +6,26 @@ import ApolloSetting from "../src/components/commons/apollo";
 import { Global } from "@emotion/react";
 import { globalStyles } from "../src/commons/styles/globalStyles";
 
+// //////////////////////firebase///////////////////////////
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZU7Qg6Xcz3Pu-bCcTcuNf6khYiKVesFk",
+  authDomain: "codecamp09-idealstring.firebaseapp.com",
+  projectId: "codecamp09-idealstring",
+  storageBucket: "codecamp09-idealstring.appspot.com",
+  messagingSenderId: "885464661436",
+  appId: "1:885464661436:web:39d269ee36b1f584803fb9",
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig); // 여기가 접속 정보임.
+// //////////////////////firebase///////////////////////////
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloSetting>
